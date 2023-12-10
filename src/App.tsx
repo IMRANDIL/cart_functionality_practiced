341fd1dbffd3284525c209b540195012d57b0c33
IMRANDIL <aliimranadil2@gmail.com>
2023-12-10 08:01:45 +0530

    added cart feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import { useState } from 'react'
 
 import './App.css'
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
 
 function App() {
  
+   // State to manage the list of products
+   const [products] = useState([
+    { id: 1, name: 'Product 1', price: 20 },
+    { id: 2, name: 'Product 2', price: 30 },
+    { id: 3, name: 'Product 3', price: 25 },
+  ]);
+
+  // State to manage the items in the cart
+  const [cart, setCart] = useState([]);
+
+    // Function to add a product to the cart
+    const addToCart = (product) => {
+      setCart([...cart, product]);
+    };
+  
+    // Function to remove a product from the cart
+    const removeFromCart = (product) => {
+      const updatedCart = cart.filter((item) => item.id !== product.id);
+      setCart(updatedCart);
+    };
 
   return (
     <>
-     
+      <div>
+      {/* Render ProductList component */}
+      <ProductList products={products} addToCart={addToCart} />
+
+      {/* Render Cart component */}
+      <Cart cart={cart} removeFromCart={removeFromCart} />
+    </div>
     </>
   )
 }
